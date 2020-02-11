a20cfefcfaa1f551ba660f41b82313a943d554b8
aldodell <aldodell@gmail.com>
2020-02-11 09:55:27 -0400

    fixes

--- kd-messages-queue.js
+++ kd-messages-queue.js
@@ -1,5 +1,1 @@
-var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 24;desktop.sendMessage(_m);
-var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 25;desktop.sendMessage(_m);
-var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 26;desktop.sendMessage(_m);
-var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 27;desktop.sendMessage(_m);
-var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 28;desktop.sendMessage(_m);
+var _m = new KDMessage();_m.importJSON({"index":0,"sourceIdentifier":"qqsm-contol","destinationIdentifier":"qqsm","values":{"show":"next"}});_m.index = 0;desktop.sendMessage(_m);
