2c920c78bad08a197638d21880e85aeecd55c601
aldodell <aldodell@gmail.com>
2020-04-06 22:33:05 -0400

    fix

--- kd-messages-queue.js
+++
@@ -1,9 +0,0 @@
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-{"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}}
-.broadcastLocalMessage(new KDMessage().import({"index":0,"sourceIdentifier":"a","destinationIdentifier":"b","values":{"saludo":"Hola mundo","agradecimiento":"¡Gracias a Dios!"}},8);
