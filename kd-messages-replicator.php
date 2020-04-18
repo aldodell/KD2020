c5c2e1709a4039f3e00890776b3c072e99aa9e10
aldodell <aldodell@gmail.com>
2020-04-18 14:34:38 -0400

    fix

--- kd-messages-replicator.php
+++ kd-messages-replicator.php
@@ -7,10 +7,22 @@
 include "kd-messages-ini.php";
 
 //Get desktop instance name
-$d = $_POST["d"];
+//$d = $_POST["d"];
 
 //Get message
 $m = $_POST["m"];
+
+//Read messages file.
+$messagesFile = file_get_contents($fileName_messages);
+
+//Split messages by return characters
+$messagesArray = preg_split("/\\r\\n/", $messagesFile);
+
+//Count messages
+$messagesQuantity = count($messagesArray);
+
+//data to save:
+$data = "";
 
 //get last id
 $id = intval(file_get_contents($fileName_index)) + 1;
@@ -18,36 +30,16 @@ $id = intval(file_get_contents($fileName_index)) + 1;
 //update id
 file_put_contents($fileName_index, $id);
 
-//Read messages file.
-$t = file_get_contents($fileName_messages);
+//Prepare a KDMessage represented as JSON and save it on database file
+$kdMessage = json_decode($m);
+$kdMessage->index = $id;
 
-//Split messages by return characters
-$y = preg_split("/\\r\\n/", $t);
+//Take last messages from files
+$j = $messagesQuantity - $MAX_MESSAGES;
+if ($j < 0) {$j = $messagesQuantity;}
+for ($i = 0; $i < $j; $i++) {$r .= $messagesArray[$i] . "\r\n";}
 
-//Count messages
-$u = count($y);
-$p = "";
-
-//Clean old messages
-if ($u > $MAX_MESSAGES) {
-    for ($i = 0; $i < $MAX_MESSAGES; $i++) {
-        $p .= $y[$u - $MAX_MESSAGES + $i - 1] . "\r\n";
-    }
-    file_put_contents($fileName_messages, "");
-
-}
-
-//build javascript with new message
-$r = $p;
-/*
-$r .= "var _m = new KDMessage();";
-$r .= "_m.importJSON($m);";
-$r .= "_m.index = $id;";
-*/
-$r .= "$d.broadcastLocalMessageWithIndex(new KDMessage().importJSON($m,$id));\r\n";
-
+$r .= json_encode($kdMessage) . "\r\n";
 
 //put the last message at end of file:
-file_put_contents($fileName_messages, $r, FILE_APPEND);
-
-?>
+file_put_contents($fileName_messages, $r);
