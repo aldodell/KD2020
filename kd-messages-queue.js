c7f36d12b3d7f88fe1829268b7b8f6eedb932751
aldodell <aldodell@gmail.com>
2020-04-18 00:26:26 -0400

    fix

--- kd-messages-queue.js
+++ kd-messages-queue.js
@@ -1,1 +1,2 @@
 desktop.broadcastLocalMessageWithIndex(new KDMessage().importJSON({"index":0,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"hola":"mundo1"}},1));
+desktop.broadcastLocalMessageWithIndex(new KDMessage().importJSON({"index":0,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"qwerty":"123456"}},2));
