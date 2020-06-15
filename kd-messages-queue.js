6a5cb5125f9bb61ed3f46c3d547661ac5aa94ffd
aldodell <aldodell@gmail.com>
2020-06-14 22:53:00 -0400

    fix

--- kd-messages-queue.js
+++ kd-messages-queue.js
@@ -1,1 +1,7 @@
 {"index":16,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"ya":"list!"}}
+{"index":17,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"abc":""}}
+{"index":18,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"abc":"123"}}
+{"index":19,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"q":"1"}}
+{"index":20,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"clave":"valor"}}
+{"index":21,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"epslex":"2"}}
+{"index":22,"sourceIdentifier":"message-sender","destinationIdentifier":"alert","values":{"a":"1"}}
